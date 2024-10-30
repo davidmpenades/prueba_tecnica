de9749e32fa5f9f539e2f1035c162084afc85a42
Davidmpenades <davidmpenades@gmail.com>
2024-10-30 16:08:57 +0100

    Frontend: Add sidebar and navigation options

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import Header from './components/layout/header'
+import Header from './components/layout/header';
+import Sidebar from './components/layout/sidebar';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
+      <BrowserRouter>
+        <div className='layout'>
+          <Sidebar />
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
